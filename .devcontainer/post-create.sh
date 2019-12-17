82f787bfbc57ec8cf32a6c4b0569667a76503c16
Jamie Cansdale <jcansdale@gmail.com>
2019-12-17 09:51:40 +0000

    Install RVM stable with ruby

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,7 @@
-sudo apt-get install -y ruby-full
+#sudo apt-get install -y ruby-full
+
+# Install RVM stable with ruby
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
 sudo gem install bundler
 bundler install
