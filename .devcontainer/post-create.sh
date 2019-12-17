04ca954df3d21ea2afad97cdcf928e060bb2bf6f
Jamie Cansdale <jcansdale@gmail.com>
2019-12-17 10:48:44 +0000

    Install Ruby 2.6

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,17 @@
 #sudo apt-get install -y ruby-full
 
 # Install RVM stable with ruby
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-sudo gem install bundler
-bundler install
+#source ~/.rvm/scripts/rvm
+#source /home/vsonline/.rvm/scripts/rvm
+
+source ~/.rvm/scripts/rvm
+type rvm | head -n 1
+rvm use 2.6
+ruby -v
+
+#sudo gem install bundler
+#bundler install
